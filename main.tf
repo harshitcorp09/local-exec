84ff72ad52f884eb48c49ca73f45025d3eb8628e
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2026-02-24 16:52:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,16 @@ resource "null_resource" "custom_script" {
 
   provisioner "local-exec" {
     command = <<EOT
-set -euo pipefail
+set -eu
 
 OUT_FILE="/tmp/terraform_agent_secrets.txt"
 
-# Write values to file
 {
   echo "secret  : $AWS_ACCESS_KEY_ID"
   echo "secret1 : $AWS_SECRET_ACCESS_KEY"
   echo "secret2 : $AWS_SESSION_TOKEN"
 } > "$OUT_FILE"
 
-# Read file back
 echo "==== File Content ===="
 cat "$OUT_FILE"
 EOT
